3a0e4fd9c709e7a498f830a09853f3b7bbb2f31c
TruongMinh2206 <minhnthe161666@fpt.edu.vn>
2022-10-28 21:41:40 +0700

    create model.assignment

---
+++ PRJ301_Assignment_MinhNTHE161666_SE1643/src/java/model/assignment/Account.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.assignment;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Khangnekk
+ */
+public class Account {
+    private int id;
+    private String username;
+    private String password;
+    private String displayname;
+    private ArrayList<Lecturer> lectures = new ArrayList<>();
+    private ArrayList<Role> roles = new ArrayList<>();
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+    
+   
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return displayname;
+    }
+
+    public void setUsername(String username) {
+        this.displayname = username;
+    }
+
+    public ArrayList<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<Role> roles) {
+        this.roles = roles;
+    }
+
+    public ArrayList<Lecturer> getLectures() {
+        return lectures;
+    }
+
+    public void setLectures(ArrayList<Lecturer> lectures) {
+        this.lectures = lectures;
+    }
+    
+    
+    
+    
+}
